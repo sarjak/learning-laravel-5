62a3e651a8ac5ca3e0aeb658c02e8ae50d68fc5e
Sarjak Gandhi <gandhi.sarjak@gmail.com>
2016-08-31 01:21:43 +0530

    Update Routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,30 @@ Route::get('home', 'HomeController@index');
 
 Route::get('blog','PagesController@index');
 
+// Reads an item
+Route::get('test',function(){
+	echo '<form action="test" method="post">';
+	echo '<input type="submit" value="submit" />';
+	echo '<input type="hidden" name="_token" value="'. csrf_token() .'" />';
+	echo '<input type="hidden" name="_method" value="DELETE" />';
+	echo '</form>';
+});
+
+// Creates an item
+Route::post('test',function(){
+	echo "This is a POST Request";
+});
+
+// Updates an item
+Route::put('test',function(){
+	echo "This is a PUT Request";
+});
+
+// Deletes an item
+Route::delete('test',function(){
+	echo "This is a DELETE Request";
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
